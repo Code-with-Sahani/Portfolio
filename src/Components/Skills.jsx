7bb63a9f94247fbf482cae41ad76ed7a7afb3e17
Rahul Sahani <111352713+Code-with-Sahani@users.noreply.github.com>
2025-01-20 15:25:15 +0530

    Update Skills.jsx

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+ import React from 'react';
 import Navbar from './Navbar';
 import { FaHtml5 } from "react-icons/fa6";
 import { FaCss3Alt } from "react-icons/fa";
@@ -21,20 +21,22 @@ const Skills = () => {
     <div className="w-full min-h-screen bg-gray-100">
       <Navbar />
       <div className="flex flex-col justify-start items-center gap-8 px-5 py-20">
-        <h1 className="w-full text-3xl font-bold text-gray-800 "style={{ textShadow: '2px 2px 6px white' }}>My Skills
+        <h1 className="w-full text-3xl font-bold text-gray-800 text-center" style={{ textShadow: '2px 2px 6px white' }}>
+          My Skills
           <span className="text-sm mt-2" style={{ textShadow: '1px 1px 4px white' }}>
-          &nbsp; Technologies I am proficient in:
+            &nbsp; Technologies I am proficient in:
           </span>
         </h1>
 
-        <div className="flex flex-wrap justify-start gap-8 w-4/5">
+        {/* Flex container with responsive wrapping */}
+        <div className="flex flex-wrap justify-center gap-8 w-full max-w-7xl">
           {skills.map((skill, index) => (
             <div
               key={index}
-              className="hover:scale-105 transform transition duration-300 bg-gradient-to-r from-purple-500 to-indigo-600 p-6 rounded-xl shadow-xl shadow-black/50 w-36 h-28 flex flex-col justify-between items-center text-center"
+              className="flex flex-col justify-center items-center text-center bg-gradient-to-r from-purple-500 to-indigo-600 p-6 rounded-xl shadow-xl shadow-black/50 w-28 sm:w-36 h-28 sm:h-32 transform transition duration-300 hover:scale-105"
             >
               {skill.icon}
-              <h2 className="text-white text-lg font-semibold mt-4">{skill.title}</h2>
+              <h2 className="text-white text-base sm:text-lg font-semibold mt-4">{skill.title}</h2>
             </div>
           ))}
         </div>
