6cbe4fc1fbe66a74a8d577f74f82c247123a273b
Rahul Sahani <111352713+Code-with-Sahani@users.noreply.github.com>
2025-01-21 13:57:01 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './Components/Home'
 import Skills from './Components/Skills'
 import Projects from './Components/Projects'
 import Certification from './Components/Certification'
 import Contact from './Components/Contact'
+import Experience from './Components/Experience'
 
 const App = () => {
   return (
@@ -12,14 +13,14 @@ const App = () => {
       <BrowserRouter>
         <Routes>
           <Route exact path="/" element={<Home />}></Route>
+          <Route path="/experience" element={<Experience/>}></Route>
           <Route path="/skills" element={<Skills />}></Route>
           <Route path="/projects" element={<Projects/>}></Route>
           <Route path="/certification" element={<Certification />}></Route>
           <Route path="/contact" element={<Contact />}></Route>
         </Routes>
       </BrowserRouter> 
-      
-       
+     
     </>
   )
 }
